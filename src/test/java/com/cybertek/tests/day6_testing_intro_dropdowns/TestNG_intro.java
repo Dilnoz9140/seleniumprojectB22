a3e4052d99f092c968b90906ce94e728f1cb049d
Dilnoza <dilnoz8605@icloud.com>
2021-06-01 23:43:16 +0500

    day4

---
+++ src/test/java/com/cybertek/tests/day6_testing_intro_dropdowns/TestNG_intro.java
@@ -0,0 +1,16 @@
+package com.cybertek.tests.day6_testing_intro_dropdowns;
+
+import org.testng.annotations.Test;
+
+public class TestNG_intro {
+
+ @Test
+    public void test1(){
+     System.out.println("Running Test 1...");
+
+    }
+@Test
+    public void test2(){
+    System.out.println("Running Test 2...");
+}
+}
